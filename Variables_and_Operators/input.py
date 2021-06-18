5a549255ad9ddd461db45e10385069b96e7447a9
viksri98 <viksri98@gmail.com>
2021-06-18 14:07:47 -0700

    update

--- Variables_and_Operators/input.py
+++ Variables_and_Operators/input.py
@@ -5,6 +5,9 @@ in_string = input("Enter a String")
 
 # in_string is a variable, holding the result of the input() function.
 # we can view its contents by clicking the "Show variables" button in Python Interactive
+
+# The "input" for the input() function is a string that is printed to the user as a prompt.
+# if you type input("string"), python will print "string" and then wait for user input
 # %%
 # we can use this variable to print out the user's input
 print(in_string)
---
+++ Variables_and_Operators/integers_floats.py
@@ -0,0 +1,37 @@
+#%%
+# Lets look at some other data types
+# integers, or ints, are whole numbers, from -2147483648 to 2147483647
+# lets store an integer
+
+a = 0
+
+#%%
+# Because python is type-inferred, we do not specify the data type of the variable
+# instead, we give the variable the data we want, and python determines its type
+# We can see the type of the variable in the variables pane in Python Interactive
+# You can also use the type() function to determine the type
+
+print(type(a))
+
+# Notice that this prints out <class 'int'>. That is because python is an object
+# oriented language, so all data types are defined in classes. We will look more
+# at objects later.
+#%%
+# floats are for decimal numbers and fractions
+f = 3.2
+# this has type float, and a value of 3.2
+#%%
+# what happens if we try 0.0?
+g = 0.0
+# g is a float, even though it is the same mathematically as 0
+# %%
+# Lets try some user input
+in_int = input("Enter an integer")
+# check its type in Python Interactive
+# Its a string! The same way 0.0 is a float and 0 is an int, "0" is a string
+#%%
+# what if we don't want it to be a string? we use the casting functions.
+int("1") # Turns any type into its equivalent integer value
+float("1") # Turns any type into its equivalent float value
+str(1) # Turns any type into its equivalent string value
+# %%
