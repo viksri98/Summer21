0cf85717f3b15892d6113de49d0a1d2d5ce49d79
viksri98 <viksri98@gmail.com>
2021-06-26 22:56:09 -0700

    Saturday night cleanup session

--- Variables_and_Operators/input.py
+++ Variables_and_Operators/input.py
@@ -1,14 +1,3 @@
-#%%
-# lets get some user input. In python, we use the input() function.
-
-in_string = input("Enter a String")
-
-# in_string is a variable, holding the result of the input() function.
-# we can view its contents by clicking the "Show variables" button in Python Interactive
-
-# The "input" for the input() function is a string that is printed to the user as a prompt.
-# if you type input("string"), python will print "string" and then wait for user input
-# %%
-# we can use this variable to print out the user's input
-print(in_string)
-# %%
+name = input("What is your name?")
+address = input("What is your address?")
+print("Hello", name, "You have a gift waiting for you at", address)
