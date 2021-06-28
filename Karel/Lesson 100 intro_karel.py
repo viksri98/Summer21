a06d9c135533f80743a047dbf7741dfa57cc17b2
surekhachandran <86130247+surekhachandran@users.noreply.github.com>
2021-06-27 18:32:03 -0700

    Add files via upload

---
+++ Karel/Lesson 100 intro_karel.py
@@ -0,0 +1,7 @@
+# This imports all Karel functions. we will look more at importing and libraries later
+from karel_robot.run import *
+
+# This controls karel's speed. It is set to 1 so you can watch karel move
+set_speed(1)
+move()
+move()
