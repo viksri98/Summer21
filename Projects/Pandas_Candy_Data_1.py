13480cc917be1d3167af416896dd58598f30f075
viksri98 <viksri98@gmail.com>
2021-07-15 10:43:21 -0700

    class thursday week 2

--- Projects/Pandas_Candy_Data_1.py
+++ Projects/Pandas_Candy_Data_1.py
@@ -12,14 +12,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 #%%import a dataframe
-candy_data = pd.read_csv('../src/candy_data.csv')
+candy_data = pd.read_csv('~/Documents/Python/Summer21/src/candy_data.csv')
 
 #%% examine the dataframe
 print(candy_data.keys())
 candy_names = candy_data['competitorname']
 win_percent = candy_data['winpercent']
 # Get the sugarpercent data into a new variable below
-
+sugar_percent = candy_data['sugarpercent']
 #%% Lets try using numpy with pandas dataframes
 # Does it work well?
 win_percent_np = np.array(win_percent)
@@ -30,3 +30,16 @@ mean_win_np = np.mean(win_percent_np)
 # convert your sugarpercent dataframe to a numpy array
 # use matplotlib to plot a graph of winpercent vs sugarpercent
 
+sugar_np = np.array(sugar_percent)
+plt.scatter(sugar_np, win_percent_np)
+plt.title("Win Percent vs Sugar Percent")
+plt.xlabel("Sugar Percent")
+plt.ylabel("Win Percent")
+plt.grid()
+#%% price vs win
+price_percent = np.array(candy_data['pricepercent'])
+plt.scatter(price_percent, win_percent_np)
+plt.title("wins vs price")
+plt.xlabel("price")
+plt.ylabel("win percent")
+plt.grid()
